51e3c7d9b2f1d268d93eb2a6f0cab661b51803e7
Balanjaneyulu13 <61181745+Balanjaneyulu13@users.noreply.github.com>
2020-02-18 13:50:47 +0530

    Create auto.java

---
+++ auto.java
@@ -0,0 +1,7 @@
+hello man!
+how's your life going
+good
+what about you man!
+just fine and iam searching for a job
+yeah i'm working in a company 
+oh! that's graet if there are openings can you help me with that?
