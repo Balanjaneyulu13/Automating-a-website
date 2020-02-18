974ebe2c54e8230cf901349de845829e3c2f72d3
Balanjaneyulu13 <61181745+Balanjaneyulu13@users.noreply.github.com>
2020-02-18 13:52:12 +0530

    Update auto.java

--- auto.java
+++ auto.java
@@ -5,3 +5,5 @@ what about you man!
 just fine and iam searching for a job
 yeah i'm working in a company 
 oh! that's graet if there are openings can you help me with that?
+yes! ofcourse. 
+Thank you
